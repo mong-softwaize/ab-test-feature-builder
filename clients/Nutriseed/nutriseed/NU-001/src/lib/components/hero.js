4d4ee2c13408a9f2ae3030e3333d69560f8fa0b1
raihankabir36850 <raihanku320@gmail.com>
2024-06-27 07:53:31 +0600

    nu001 done for review

--- clients/Nutriseed/nutriseed/NU-001/src/lib/components/hero.js
+++ clients/Nutriseed/nutriseed/NU-001/src/lib/components/hero.js
@@ -3,13 +3,11 @@ export const hero = (id) => {
         <div class="ns-20-main-container ${id}__hero-section">
             <div class="ns-20-text-wrapper">
                 <div>
-
                     <h1 class="header-main-message large">
                         <small>Healthy living made easy</small>
                         <strong>Reach your goals</strong>
                         <span>and stay there</span>
                     </h1>
-
                     <p class="header-secondary-message">
                         Kickstart weight-loss, reset your healthy habits &amp; feel your best. Our natural juices are packed
                         with fruit and veg to get you feeling healthier, cleanse your body, crush your cravings and keep you on
@@ -23,9 +21,15 @@ export const hero = (id) => {
                         healthier.</p>
                 </div>
                 <div class="ns-20-mobile-anchor">
-                    <a href="https://www.nutriseed.co.uk/pages/52-club">Join the 5:2 Club </a>
+                    <a href="https://www.nutriseed.co.uk/pages/52-club" class="button--with-hand">Join the 5:2 Club </a>
                     <a href="/products/cold-pressed-juice-cleanse"
-                    class="shop-juice">Shop Juice Cleanse</a>
+                    class="shop-juice button--with-hand lpg-button ns-11-1-cta">Shop Juice Cleanse</a>
+                    <div class="${id}__reviews">
+                        <div class="${id}__text">Excellent</div>
+                        <div class="${id}__image">
+                            <img src="https://storage.googleapis.com/abtest-img-bucket/trust-banner.png"/>
+                        </div>
+                    </div>
                 </div>
                 <div class="${id}__button-container">
                     <div >
@@ -34,7 +38,14 @@ export const hero = (id) => {
                     <div>
                         <a href="/products/cold-pressed-juice-cleanse" class="ns-20-shop-juice-CTA ${id}__shop-juice lpg-button button--with-hand ns-11-1-cta">Shop Juice Cleanse</a>
                     </div>
+                    <div class="${id}__reviews">
+                        <div class="${id}__text">Excellent</div>
+                        <div class="${id}__image">
+                            <img src="https://storage.googleapis.com/abtest-img-bucket/trust-banner.png"/>
+                        </div>
+                    </div>
                 </div>
+                
             </div>
             <div>
 
@@ -45,14 +56,14 @@ export const hero = (id) => {
                 </h1>
                 <img src="https://storage.googleapis.com/abtest-img-bucket/NS%2341-banner-desktop.png" class="${id}__desktop-image">
                 <div class="${id}__mobile-image-wrapper">
-                <img src="https://storage.googleapis.com/abtest-img-bucket/NS%2341-banner-mobile.png" class="${id}__mobile-image">
+                    <img src="https://storage.googleapis.com/abtest-img-bucket/NS%2341-banner-mobile.png" class="${id}__mobile-image">
+                    <div class="custom-shape-divider-bottom-1719447840">
+                        <svg data-name="Layer 1" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1200 120" preserveAspectRatio="none">
+                            <path d="M321.39,56.44c58-10.79,114.16-30.13,172-41.86,82.39-16.72,168.19-17.73,250.45-.39C823.78,31,906.67,72,985.66,92.83c70.05,18.48,146.53,26.09,214.34,3V0H0V27.35A600.21,600.21,0,0,0,321.39,56.44Z" class="shape-fill"></path>
+                        </svg>
+                    </div>   
                 </div>
             </div>
-
-
-            <svg class="svg">
-  <clipPath id="my-clip-path" clipPathUnits="objectBoundingBox"><path d="M0,0.068 C0,0.068,0.119,0.109,0.261,0.109 C0.393,0.109,0.576,-0.017 e-05,236.611,0, C0.848,0.015 e-05,328,5.80205,328,5.80205, V1 H0 V0.068"></path></clipPath>
-</svg>
         </div>
     `;
   return html;
