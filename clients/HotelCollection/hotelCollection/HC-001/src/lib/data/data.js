c4862c095b9d3319c92de4713d836a5170396518
raihankabir36850 <raihanku320@gmail.com>
2024-07-05 13:05:03 +0600

    data arrange properly except two catagories and css chnages

--- clients/HotelCollection/hotelCollection/HC-001/src/lib/data/data.js
+++ clients/HotelCollection/hotelCollection/HC-001/src/lib/data/data.js
@@ -1,44 +1,46 @@
 export const categoryData = [
   {
     title: 'scent diffusers',
-    image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
-    url: '',
+    image: 'https://www.hotelcollection.com/cdn/shop/files/wirelesspro-myway.png?v=1708708169&width=640',
+    url: '/collections/scent-diffusers',
     alt: 'scent diffusers',
   },
   {
     title: 'Scent diffuser&nbsp;oils',
-    image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
+    image: 'https://www.hotelcollection.com/cdn/shop/files/wirelesspro-myway.png?v=1708708169&width=640',
     url: '',
     alt: 'Scent diffuser oils',
   },
   {
     title: 'chauffeur car&nbsp;diffuser',
-    image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
-    url: '',
+    image: 'https://www.hotelcollection.com/cdn/shop/files/Car-Diffuser.png?v=1690529341&width=600',
+    url: '/collections/car-diffuser-oil',
     alt: 'chauffeur cardiffuser',
   },
   {
     title: 'room <br>sprays',
     image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
-    url: '',
+    url: '/collections/room-sprays',
     alt: 'room sprays',
   },
   {
     title: 'reed diffusers',
-    image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
-    url: '',
+    image:
+      'https://www.hotelcollection.com/cdn/shop/files/Reed-Diffusers_03a43b7f-c255-48a1-8fc3-02238e70e0e1.png?v=1690529359&width=600',
+    url: '/collections/reed-diffusers',
     alt: 'reed diffusers',
   },
   {
     title: 'candle <br>sets',
-    image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
-    url: '',
+    image:
+      'https://www.hotelcollection.com/cdn/shop/files/Candles_a9eba6ef-1e3e-4797-bc35-493789e0fbfe.png?v=1690529452&width=588',
+    url: '/collections/all-candles',
     alt: 'candle sets',
   },
   {
     title: 'hourglass diffuser',
-    image: 'https://www.hotelcollection.com/cdn/shop/files/candles.jpg?v=1690361415&width=500',
-    url: '',
+    image: 'https://www.hotelcollection.com/cdn/shop/files/Hourglass-Diffuser.png?v=1690529397&width=588',
+    url: '/collections/hourglass-diffuser-and-oils',
     alt: 'hourglass diffuser',
   },
 ];
