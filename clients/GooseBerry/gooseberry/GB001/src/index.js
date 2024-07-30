04dd3654c9aa176b62dc2bf203d2bb121b53bb5f
raihankabir36850 <raihanku320@gmail.com>
2024-07-30 11:38:46 +0600

    add a class for checking

--- clients/GooseBerry/gooseberry/GB001/src/index.js
+++ clients/GooseBerry/gooseberry/GB001/src/index.js
@@ -1,4 +1,4 @@
 import activate from './lib/experiment';
 import { pollerLite } from './lib/helpers/utils';
 
-pollerLite(['body', '.model-size-dropdown', '.product__color-chips'], activate);
+pollerLite(['body.template-product', '.model-size-dropdown', '.product__color-chips'], activate);
