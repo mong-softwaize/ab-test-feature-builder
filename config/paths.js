4913877abe004f1808a73ff4cf8aed773f2fd472
raihankabir36850 <raihanku320@gmail.com>
2024-08-08 13:30:04 +0600

    create: new test: PQ002

--- config/paths.js
+++ config/paths.js
@@ -4,13 +4,13 @@
     module.exports = {
       // Source files
     
-      src: path.resolve(__dirname, `../clients/Pequeayuda/pequeayuda/PQ001/src`),
+      src: path.resolve(__dirname, `../clients/Pequeayuda/pequeayuda/PQ002/src`),
     
       // Production build files
-      build: path.resolve(__dirname, `../clients/Pequeayuda/pequeayuda/PQ001/public`),
+      build: path.resolve(__dirname, `../clients/Pequeayuda/pequeayuda/PQ002/public`),
     
       // Static files that get copied to build folder
-      public: path.resolve(__dirname, `../clients/Pequeayuda/pequeayuda/PQ001/public`),
+      public: path.resolve(__dirname, `../clients/Pequeayuda/pequeayuda/PQ002/public`),
     
       templateHTML: path.resolve(__dirname, '../template.html'),
     };
